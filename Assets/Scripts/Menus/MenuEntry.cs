f6a23857ea3de1f8954aa2d4012d7f3627a104d1
Markus Peterson <marcus17777@gmail.com>
2019-10-05 18:17:04 +0300

    WIP. Menus

---
+++ Assets/Scripts/Menus/MenuEntry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class MenuEntry : MonoBehaviour
+{
+    public String title;
+    private TextMeshProUGUI _textField;
+    private bool _active;
+    private MenuAction _menuAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _textField = gameObject.GetComponent<TextMeshProUGUI>();
+        _menuAction = gameObject.GetComponent<MenuAction>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _textField.text = title;
+        _textField.fontStyle = _active ? FontStyles.Bold : FontStyles.Normal;
+    }
+
+    public void SetActive()
+    {
+        _active = true;
+    }
+
+    public void SetInactive()
+    {
+        _active = false;
+    }
+
+    public void Select()
+    {
+        if (_menuAction)
+        {
+            _menuAction.doAction();
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/MenuPage.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPage : MonoBehaviour
+{
+    private MenuEntry[] _menuEntries;
+    private int _activeIndex;
+
+    private MenuEntry Active => _menuEntries[_activeIndex];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _menuEntries = GetComponentsInChildren<MenuEntry>();
+        _activeIndex = 0;
+        Active.SetActive();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ApplicationSettings.IsPaused()) return;
+        
+        HandleActions();   
+    }
+
+    void HandleActions()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            IncrementIndex();
+        } else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            DecrementIndex();
+        } else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Active.Select();
+        }
+        
+    }
+
+    void IncrementIndex()
+    {
+        if (_activeIndex >= _menuEntries.Length - 1) return;
+        
+        Active.SetInactive();
+        _activeIndex += 1;
+        Active.SetActive();
+    }
+    
+    void DecrementIndex()
+    {
+        if (_activeIndex <= 0) return;
+        
+        Active.SetInactive();
+        _activeIndex -= 1;
+        Active.SetActive();
+    }
+}
