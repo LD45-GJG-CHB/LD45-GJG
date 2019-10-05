2419a8583171477e60ba1310c1b766ffa96263be
Markus Peterson <marcus17777@gmail.com>
2019-10-05 19:45:24 +0300

    Menu

---
+++ Assets/Scripts/Menus/SwitchPageAction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class SwitchPageAction : MenuAction
+{
+    public MenuPage Page;
+    public MenuManager MenuManager;
+
+    public override void doAction()
+    {
+        MenuManager.Switch(Page);
+    }
+}
---
+++ Assets/Scripts/Menus/SwitchSceneAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+
+public class SwitchSceneAction : MenuAction
+{
+    public String switchTo;
+    
+    public override void doAction()
+    {
+        SceneManager.LoadScene(switchTo, LoadSceneMode.Single);
+    }
+}
