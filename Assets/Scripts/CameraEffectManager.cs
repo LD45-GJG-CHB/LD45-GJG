0516e3e7f263ff5ac17c6470ded123727e1a3ed5
SiiMeR <siimraudsepp@msn.com>
2019-10-05 19:33:01 +0300

    Trippy effecx

--- Assets/Scripts/CameraEffectManager.cs
+++ Assets/Scripts/CameraEffectManager.cs
@@ -5,19 +5,15 @@ public class CameraEffectManager : MonoBehaviour
 {
     public Shader trippyShader;
 
-    private Material _trippyMat;
+    public Material trippyMat;
 
-
-    private void Awake()
-    {
-        _trippyMat = new Material(trippyShader){hideFlags = HideFlags.HideAndDontSave};
-    }
+    
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
         var rt = RenderTexture.GetTemporary(src.width, src.height, 0, src.format);
 
-        Graphics.Blit(src, dest, _trippyMat);
+        Graphics.Blit(src, dest, trippyMat);
         
         RenderTexture.ReleaseTemporary(rt);
     }
