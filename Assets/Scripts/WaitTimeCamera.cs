d33071e2a442f64e22962443304e38f8ce7194a0
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2019-10-06 16:10:03 +0300

    fixed virtual camera

--- Assets/Scripts/WaitTimeCamera.cs
+++ Assets/Scripts/WaitTimeCamera.cs
@@ -11,7 +11,7 @@ public class WaitTimeCamera : Singleton<WaitTimeCamera>
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
