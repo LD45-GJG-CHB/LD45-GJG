0d78060b180038910001c99376d653da480bbf4a
Markus Peterson <marcus17777@gmail.com>
2019-10-06 21:34:26 +0300

    menu fixeas

---
+++ Assets/Scripts/Menus/BackButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Update is called once per frame
+
+    private Button _buttonComponent;
+
+    private void Start()
+    {
+        _buttonComponent = gameObject.GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _buttonComponent.onClick.Invoke();
+        }
+    }
+}
