6a46506c032a50a319a905045346b3981492c49e
Markus Peterson <marcus17777@gmail.com>
2019-10-06 14:33:04 +0300

    add difficulty options

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,8 +1,10 @@
 using System;
+using DefaultNamespace;
 
 public static class GameState
 {
     public static bool IsPlayerDead;
     public static String playerName;
     public static Highscore playerLastHighscore;
+    public static Difficulty Difficulty;
 }
---
+++ Assets/Scripts/Menus/SetDifficultyAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class SetDifficultyAction : MenuAction
+{
+    public Difficulty Difficulty;
+    
+    public override void doAction()
+    {
+        GameState.Difficulty = Difficulty;
+    }
+}
