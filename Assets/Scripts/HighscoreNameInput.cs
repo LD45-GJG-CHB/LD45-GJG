9ab6f177cdbc737e318dc58247e7fe633a789a9e
Markus Peterson <marcus17777@gmail.com>
2019-10-07 16:33:43 +0300

    removed unused inmports

--- Assets/Scripts/HighscoreNameInput.cs
+++ Assets/Scripts/HighscoreNameInput.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class HighscoreNameInput : MonoBehaviour
 {
--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UI;
-using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
+﻿using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
